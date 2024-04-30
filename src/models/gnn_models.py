810a0bc6eef293f806ea436e92ca7fd9689ae004
nklsKrmnn <stud103746@fh-wedel.de>
2024-04-30 14:05:53 +0200

    GCNN with readout

--- src/models/gnn_models.py
+++ src/models/gnn_models.py
@@ -8,12 +8,13 @@ class TestGNN(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, device):
         super(TestGNN, self).__init__()
         self.conv1 = GCNConv(input_dim, hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, output_dim)
-        #TODO put a dense layer after (read out: FC layer notewise)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.readout = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
+        x = self.readout(x)
         return F.softmax(x, dim=1)
