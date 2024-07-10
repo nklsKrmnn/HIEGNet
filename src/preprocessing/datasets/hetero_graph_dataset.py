d4a2b9c7917fe3d393c69da5504d43259b945c02
nklsKrmnn <stud103746@fh-wedel.de>
2024-07-10 14:00:48 +0200

    Removed old processing from hetero_graph_dataset.py

--- src/preprocessing/datasets/hetero_graph_dataset.py
+++ src/preprocessing/datasets/hetero_graph_dataset.py
@@ -1,3 +1,4 @@
+from typing import Final
 import cv2
 import pandas as pd
 import numpy as np
@@ -9,181 +10,109 @@ from sklearn.utils import compute_class_weight
 from torch_geometric.data import Data, HeteroData
 from sklearn.model_selection import StratifiedKFold, train_test_split
 
+from src.preprocessing.datasets.dataset_utils.dataset_utils import get_train_val_test_indices
 from src.preprocessing.feature_preprocessing import feature_preprocessing
 from src.preprocessing.graph_preprocessing.knn_graph_constructor import knn_graph_construction, \
-    knn_weighted_graph_construction
-from src.utils.path_io import get_glom_index
+    knn_weighted_graph_construction, graph_construction
 from src.preprocessing.datasets.glom_graph_dataset import GlomGraphDataset
+from src.utils.path_io import get_path_up_to
+
+ROOT_DIR: Final[str] = get_path_up_to(os.path.abspath(__file__), "repos")
 
 
 class HeteroGraphDataset(GlomGraphDataset):
     def __init__(self, **kwargs):
-        self.cell_node_dir_path = kwargs.pop('cell_node_dir_path')
         self.cell_types = kwargs.pop('cell_types')
-        self.n_neighbours_cells = kwargs.pop('n_neighbours_cells')
+        self.cell_graph_params = kwargs.pop('cell_graph')
+
+        self.cell_node_files = [os.path.join(kwargs.pop('cell_node_dir_path'), f"{cell_type}_cell_nodes.pkl") for
+                                cell_type in
+                                self.cell_types]
 
         super().__init__(**kwargs)
 
-    def process(self) -> None:
+    def create_graph_object(self, df_patient) -> HeteroData:
         """
-        Process the raw data to the graph data format.
+        Create the graph object from the raw data.
 
         The raw data is loaded from the raw file and a graph is constructed from the point cloud with the knn graph
         algorithm. Afterward, node features and note-wise targets for node classification are added. A train and test
-        mask is added to the graph data object. Finally, the data object is saved to the processed directory.
+        mask is added to the graph data object.
 
-        :return: None
+        :return: The graph data object
         """
-        df = pd.read_csv(self.raw_paths[0])
-        df_annotations = pd.concat([pd.read_csv(path) for path in self.raw_paths[1:]])
-        df = pd.merge(df, df_annotations, left_on="glom_index", right_on="ID", how="left")
-        patients_in_raw_data = df['patient'].unique()
+        patient = df_patient['patient'].iloc[0]
 
-        cell_node_files = [os.path.join(self.cell_node_dir_path, f"{cell_type}_cell_nodes.pkl") for cell_type in
-                           self.cell_types]
-        list_cell_nodes = [pd.read_pickle(path) for path in cell_node_files]
+        data = HeteroData()
 
-        file_names = []
+        # Target labels
+        data.target_labels = ['Term_Healthy', 'Term_Sclerotic', 'Term_Dead']
 
-        for patient in patients_in_raw_data:
-            df_patient = df[df['patient'] == patient]
+        # Create the graph from point cloud and generate the edge index
+        coords = df_patient[['Center X', 'Center Y']].to_numpy()
+        edge_index, edge_weights = graph_construction(coords, **self.glom_graph)
+        data[('glomeruli', 'to', 'glomeruli')].edge_index = torch.tensor(edge_index, dtype=torch.long)
+        data[('glomeruli', 'to', 'glomeruli')].edge_attr = torch.tensor(edge_weights, dtype=torch.float).unsqueeze(1)
 
-            # Drop rows where feature or image path is missing (most likely because no match through slices)
-            df_patient.dropna(subset=self.feature_list, inplace=True)
+        y = self.create_targets(df_patient, data.target_labels)
 
-            # threshold for minimum number of data points and check if patient is in train or test set
-            if (df_patient.shape[0] > 10) and (
-                    patient in self.train_patients + self.validation_patients + self.test_patients):
+        # Generate stratified train, val and test indices
+        train_indices, val_indices, test_indices = get_train_val_test_indices(y, self.test_split,
+                                                                              self.val_split,
+                                                                              self.random_seed,
+                                                                              self.test_patients,
+                                                                              self.validation_patients)
 
-                # Create the data object for each graph
-                data = HeteroData()
+        data.train_mask = self.create_mask(len(y), train_indices)
+        data.val_mask = self.create_mask(len(y), val_indices)
+        data.test_mask = data.val_mask if (self.test_split == 0.0) and (self.test_patients == []) else self.create_mask(
+            len(y), test_indices)
 
-                # Target labels
-                data.target_labels = ['Term_Healthy', 'Term_Sclerotic', 'Term_Dead']
+        # Create the node features in tensor
+        data["glomeruli"].x = self.create_feature_tensor(df_patient, train_indices, self.feature_list)
 
-                # Create the graph from point cloud and generate the edge index
-                coords = df_patient[['Center X', 'Center Y']].to_numpy()
-                adjectency_matrix = knn_graph_construction(coords, self.n_neighbours)
-                edge_index = torch.tensor(np.argwhere(adjectency_matrix == 1).T, dtype=torch.long)
-                data[('glomeruli', 'to', 'glomeruli')].edge_index = edge_index
+        data.y = y
 
-                # Create the node features in tensor
-                if self.path_image_inputs is None:
-                    # Get numerical features
-                    x = df_patient[self.feature_list]
-                    x = torch.tensor(x.to_numpy(), dtype=torch.float)
-                else:
-                    # Get image paths
-                    x = [[df_patient[path].iloc[i] for path in self.feature_list] for i in range(df_patient.shape[0])]
-                data['glomeruli'].x = x
+        data.glom_indices = torch.tensor(df_patient['glom_index'].values)
+        data.coords = torch.tensor(coords, dtype=torch.float)
 
-                # Create the target labels in tensor
-                if self.onehot_targets:
-                    df_patient = pd.get_dummies(df_patient, columns=['Term'])
-                    # Add missing target columns if not represented in the data
-                    for target in data.target_labels:
-                        if target not in df_patient.columns:
-                            df_patient[target] = False
-                    y = df_patient[data.target_labels]
-                    y = torch.tensor(y.to_numpy(), dtype=torch.float)
-                else:
-                    y = df_patient['Term']
-                    y.replace({'Healthy': 0, 'Sclerotic': 1, 'Dead': 2}, inplace=True)
-                    y = torch.tensor(y.to_numpy(), dtype=torch.long)
-                data.y = y
+        list_cell_nodes = [pd.read_pickle(file) for file in self.cell_node_files]
 
-                data.glom_indices = torch.tensor(df_patient['glom_index'].values)
-                data.coords = torch.tensor(coords, dtype=torch.float)
-                data.adjacency_matrix = torch.tensor(adjectency_matrix, dtype=torch.float)
+        # Add other cells to graph
+        for i, df_cell_nodes in enumerate(list_cell_nodes):
+            df_cell_nodes = df_cell_nodes[df_cell_nodes['patient'] == patient].reset_index(drop=True)
 
-                # Generate stratified train, val and test indices
-                # Test set becomes equal to val set, if test split is 0
-                # Val set becomes equal to train set, if val split is 0
-                if (self.test_split < 1.0 and self.test_split > 0.0):
-                    train_indices, test_indices = train_test_split(np.arange(len(y)),
-                                                                   test_size=float(self.test_split),
-                                                                   random_state=self.random_seed,
-                                                                   stratify=y.numpy())
-                    # Add a correction, because 100% for the val split will be reduced by the test split
-                    val_split_correction = self.test_split * self.val_split
-                else:
-                    train_indices = np.arange(len(y))
-                    test_indices = np.arange(len(y)) if patient in self.test_patients else np.array([])
-                    val_split_correction = 0
-                if (self.val_split < 1.0 and self.val_split > 0.0):
-                    train_indices, val_indices = train_test_split(train_indices,
-                                                                  test_size=float(
-                                                                      self.val_split + val_split_correction),
-                                                                  random_state=self.random_seed,
-                                                                  stratify=y[train_indices].numpy())
-                else:
-                    train_indices = np.arange(len(y))
-                    val_indices = np.arange(len(y)) if patient in self.val_patients else np.array([])
+            # Create connections between cells
+            cell_coords = df_cell_nodes[['center_x_global', 'center_y_global']].to_numpy()
+            cell_edge_index, cell_edge_weight = graph_construction(cell_coords, **self.cell_graph_params)
+            data[self.cell_types[i], 'to', self.cell_types[i]].edge_index = torch.tensor(cell_edge_index,
+                                                                                         dtype=torch.long)
+            data[self.cell_types[i], 'to', self.cell_types[i]].edge_attr = torch.tensor(cell_edge_weight,
+                                                                                        dtype=torch.float).unsqueeze(1)
 
-                # Add train, val and test masks based on random seed and split values
-                data.train_mask = torch.zeros(data['glomeruli'].num_nodes, dtype=torch.bool)
-                data.train_mask[train_indices] = True
-                data.val_mask = torch.zeros(data['glomeruli'].num_nodes, dtype=torch.bool)
-                data.val_mask[val_indices] = True
-                data.test_mask = torch.zeros(data['glomeruli'].num_nodes, dtype=torch.bool)
-                data.test_mask[test_indices] = True
-                data.test_mask = data.val_mask if (self.test_split == 0.0) and (
-                        self.test_patients == []) else data.test_mask
+            # Create connections between cells and glomeruli
+            df_cell_nodes['cell_row'] = np.arange(df_cell_nodes.shape[0])
+            df_cells_exploded = df_cell_nodes.explode('associated_glomeruli')
+            df_cells_exploded['glom_index'] = df_cells_exploded['associated_glomeruli'].apply(lambda d: d['glom_index'])
+            df_cells_exploded['distance'] = df_cells_exploded['associated_glomeruli'].apply(lambda d: d['distance'])
+            df_glom_connection = df_patient['glom_index'].to_frame()
+            df_glom_connection['glom_row'] = np.arange(df_glom_connection.shape[0])
+            df_glom_connection = df_cells_exploded.merge(df_glom_connection, right_on='glom_index',
+                                                         left_on='glom_index', how='inner')
+            cell_glom_edge_index = (df_glom_connection['cell_row'], df_glom_connection['glom_row'])
+            cell_glom_edge_weights = torch.tensor(df_glom_connection['distance'].values, dtype=torch.float).unsqueeze(1)
 
-                # Preprocessing after determining train mask
-                if self.path_image_inputs is None:
-                    x = pd.DataFrame(data['glomeruli'].x.numpy())
-                    x = feature_preprocessing(x, train_indices, **self.preprocessing_params)
-                    data['glomeruli'].x = torch.tensor(x.to_numpy(), dtype=torch.float)
+            data[self.cell_types[i], 'to', 'glomeruli'].edge_index = torch.tensor(cell_glom_edge_index,
+                                                                                  dtype=torch.long)
+            data[self.cell_types[i], 'to', 'glomeruli'].edge_attr = cell_glom_edge_weights
 
-                # Add other cells to graph
-                for i, df_cell_nodes in enumerate(list_cell_nodes):
-                    df_cell_nodes = df_cell_nodes[df_cell_nodes['patient'] == patient].reset_index(drop=True)
+            # Get node features
+            data[self.cell_types[i]].x = self.create_feature_tensor(df=df_cell_nodes,
+                                                                    train_indices=list(range(0, len(df_cell_nodes))),
+                                                                    feature_list=[c for c in df_cell_nodes.columns if
+                                                                                  c.endswith('_node_feature')])
 
-                    # Create connections between cells
-                    cell_coords = df_cell_nodes[['center_x_global', 'center_y_global']].to_numpy()
-                    cell_sparse_matrix = knn_weighted_graph_construction(cell_coords, self.n_neighbours_cells)
-
-                    cell_edge_index = torch.tensor(cell_sparse_matrix.nonzero(), dtype=torch.long)
-                    cell_edge_weight = torch.tensor(cell_sparse_matrix.data, dtype=torch.float).unsqueeze(1)
-                    data[self.cell_types[i], 'to', self.cell_types[i]].edge_index = cell_edge_index
-                    data[self.cell_types[i], 'to', self.cell_types[i]].edge_attr = cell_edge_weight
-
-                    # Create connections between cells and glomeruli
-                    df_cell_nodes['cell_row'] = np.arange(df_cell_nodes.shape[0])
-                    df_cells_exploded = df_cell_nodes.explode('associated_glomeruli')
-                    df_cells_exploded['glom_index'] = df_cells_exploded['associated_glomeruli'].apply(lambda d: d['glom_index'])
-                    df_cells_exploded['distance'] = df_cells_exploded['associated_glomeruli'].apply(lambda d: d['distance'])
-                    df_glom_connection = df_patient['glom_index'].to_frame()
-                    df_glom_connection['glom_row'] = np.arange(df_glom_connection.shape[0])
-                    df_glom_connection = df_cells_exploded.merge(df_glom_connection, right_on='glom_index',
-                                                             left_on='glom_index', how='inner')
-                    cell_glom_edge_index = (df_glom_connection['cell_row'], df_glom_connection['glom_row'])
-                    cell_glom_edge_weights = torch.tensor(df_glom_connection['distance'].values, dtype=torch.float).unsqueeze(1)
-
-                    data[self.cell_types[i], 'to', 'glomeruli'].edge_index = torch.tensor(cell_glom_edge_index,
-                                                                                          dtype=torch.long)
-                    data[self.cell_types[i], 'to', 'glomeruli'].edge_attr = cell_glom_edge_weights
-
-                    # Get node features
-                    x = df_cell_nodes[[c for c in df_cell_nodes.columns if c.endswith('_node_feature')]]
-                    x = feature_preprocessing(x, list(range(0, len(x))), **self.preprocessing_params)
-                    x = torch.tensor(x.to_numpy(), dtype=torch.float)
-
-                    # Save cell data in graph object
-                    data[self.cell_types[i]].x = x
-
-                # Save graph data object
-                file_name = f"{self.processed_file_name}_p{patient}.pt"
-                torch.save(data, os.path.join(self.processed_dir, file_name))
-                print(f'[Dataset]: Saves {file_name}')
-
-                # Save file names of processed files and if patient is in train/test set and save in settings dict
-                set = 'train' if patient in self.train_patients else 'validation' if patient in self.validation_patients else 'test'
-                file_names.append({"file_name": file_name, "set": set})
-
-        with open(os.path.join(self.processed_dir, f"{self.processed_file_name}_filenames.pkl"), 'wb') as handle:
-            pickle.dump(file_names, handle)
+        return data
 
     def get(self, idx):
         item = torch.load(os.path.join(self.processed_dir, self.processed_file_names[idx]))
@@ -192,7 +121,7 @@ class HeteroGraphDataset(GlomGraphDataset):
         if not isinstance(item['glomeruli'].x[0], torch.Tensor):
             images = []
             for paths in item.x:
-                slices=[]
+                slices = []
                 for slice in paths:
                     slices.append(cv2.imread(slice))
                 img = np.concatenate(slices, axis=2)
