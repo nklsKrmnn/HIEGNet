167bb1301a537a3e13b5623e53762476c1978486
nklsKrmnn <stud103746@fh-wedel.de>
2024-07-18 10:52:00 +0200

    smaller search space

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -46,10 +46,10 @@ TRAIN_PARAMETER_SEARCH_SPACE: Final[dict[str, list]] = {
 }
 
 MODEL_PARAMETER_SEARCH_SPACE: Final[dict[str, list]] = {
-    'hidden_dim': [16, 32, 64, 128],
-    'n_message_passings': [1, 2, 3, 5, 10],
+    'hidden_dim': [16, 32, 64],
+    'n_message_passings': [1, 3, 5, 10],
     'n_fc_layers': [1, 2, 3],
-    'dropout': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+    'dropout': [0.3, 0.5, 0.6, 0.7, 0.8, 0.9],
     'softmax_function': ['softmax', 'log_softmax', 'none'],
     'msg_passing_types': [{k: t[i] for i, k in enumerate(["glom_to_glom", "cell_to_glom", "cell_to_cell"])} for t in
                           list(product(*[['gat_v2', 'gcn', 'gin'] for i in range(3)]))],
