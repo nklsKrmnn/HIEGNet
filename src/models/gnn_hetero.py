a63363873c71b1d5c60366264cef9fafe78530b5
nklsKrmnn <stud103746@fh-wedel.de>
2024-08-08 13:50:32 +0200

    Implemented GCN for hetero edges

--- src/models/gnn_hetero.py
+++ src/models/gnn_hetero.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import HeteroConv
@@ -5,6 +6,8 @@ from torch_geometric.nn import HeteroConv
 from src.models.model_constants import MESSAGE_PASSING_MAPPING
 from src.models.model_utils import init_norm_layer
 
+def generate_helper_node_type(edge_type):
+    return f'{edge_type[0]}->{edge_type[2]}'
 
 class HeteroMessagePassingLayer(nn.Module):
     def __init__(self, output_dim, edge_types, dropout=0.5, norm: str = None):
@@ -53,6 +56,11 @@ class HeteroMessagePassingLayer(nn.Module):
             if params == {}:
                 raise ValueError(f"Message passing type {msg_passing_type} not supported.")
 
+            # Adjust edge type for gcn on hetero edges to enable special handling in forward pass
+            if (msg_passing_type == "gcn") and (edge_type[0] != edge_type[2]):
+                helper_node_type = generate_helper_node_type(edge_type)
+                edge_type = (helper_node_type, 'to', helper_node_type)
+
             # Initialize message passing layer
             message_passing_class = MESSAGE_PASSING_MAPPING[msg_passing_type]
             hetero_conv_dict[edge_type] = message_passing_class(**params)
@@ -75,8 +83,23 @@ class HeteroMessagePassingLayer(nn.Module):
                 input_msg_passing['edge_attr_dict'].update({edge_type: edge_attr_dict[edge_type]})
             if msg_passing_type == "gine":
                 input_msg_passing['edge_attr_dict'].update({edge_type: edge_attr_dict[edge_type]})
-            if msg_passing_type == "gcn":
+            if msg_passing_type in ["gcn", "rgcn"]:
                 input_msg_passing['edge_weight_dict'].update({edge_type: edge_attr_dict[edge_type]})
+            if msg_passing_type == "rgcn":
+                # Insert edge type tensor (required for rgcn class)
+                input_msg_passing['edge_type'].update({edge_type: torch.zeros(edge_attr_dict[edge_type].shape[0])})
+
+            if (msg_passing_type == "gcn") and ("->" in edge_type[0]):
+                edge_source = edge_type[0].split("->")[0]
+                edge_target = edge_type[0].split("->")[1]
+                # Concat feature vectors into one tensor for gcn on hetero edges
+                input_msg_passing['x_dict'].update({edge_type: torch.cat([x_dict[edge_source], x_dict[edge_target]])})
+                # Adjust edge index for concatenated feature vectors
+                input_msg_passing['edge_index_dict'].update(
+                    {edge_type:
+                         torch.cat([edge_index_dict[edge_type][0].unsqueeze(0),
+                                    edge_index_dict[edge_type][1].unsqueeze(0) + x_dict[edge_type[0]].shape[0]], dim=0)
+                     })
 
         x_dict = self.message_passing_layer(**input_msg_passing)
         x_dict = {key: self.norm(x) for key, x in x_dict.items()}
@@ -116,7 +139,10 @@ class HeteroGNN(nn.Module):
         for cell_type in cell_types:
             edge_types[(cell_type, 'to', 'glomeruli')] = msg_passing_types['cell_to_glom']
             for cell_type2 in cell_types:
-                edge_types[(cell_type, 'to', cell_type2)] = msg_passing_types['cell_to_cell']
+                if (cell_type != cell_type2) and (msg_passing_types['cell_to_cell'] == 'gcn'):
+                    edge_types[(cell_type, 'to', cell_type2)] = 'gcn' #TODO RGCN
+                else:
+                    edge_types[(cell_type, 'to', cell_type2)] = msg_passing_types['cell_to_cell']
         node_types = ['glomeruli'] + cell_types
 
         # FC layer to unify input dimensions
