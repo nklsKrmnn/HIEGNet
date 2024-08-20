0b3d1d08f6adc16ca2dda95137b92af652dec82e
nklsKrmnn <kormann8@icloud.com>
2024-08-20 19:35:49 +0200

    changes on gpu

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -35,16 +35,16 @@ REFERENCE_POINTS: Final[dict[str, dict[str, tuple[float, float]]]] = {
 }
 
 PARAMETER_SEARCH_SPACE: Final[dict[str, list]] = {
-    'hidden_dim': [16, 32, 64],
+    'hidden_dim': [32, 64],
     'n_message_passings': [1, 2, 3, 4, 5],
-    'n_fc_layers': [1, 2, 3],
+    'n_fc_layers': [1, 2],
     'dropout': [0.1, 0.2, 0.3, 0.4, 0.5],
     'softmax_function': ['softmax', 'log_softmax', 'none'],
     'norm_fc_layers': ['batch', 'layer', 'none'],
     'norm': ['batch', 'layer', 'none'],
-    'glom_to_glom': ['gcn', 'gat_v2', 'gine'],
-    'cell_to_glom': ['gcn', 'gat_v2', 'gine'],
-    'cell_to_cell': ['gcn', 'gat_v2', 'gine'],
+    'glom_to_glom': ['gcn'],
+    'cell_to_glom': ['gcn', 'gat_v2', 'gine', 'sage'],
+    'cell_to_cell': ['gcn', 'gat_v2', 'gine', 'sage'],
     "learning_rate": [0.003, 0.001, 0.0003, 0.0001]
 }
 MODEL_NAME_MAPPING: Final[dict[str, any]] = {
