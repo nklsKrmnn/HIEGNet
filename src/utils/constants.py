172f867c91154041d8a83f58735559137c50f419
nklsKrmnn <stud103746@fh-wedel.de>
2025-02-04 10:34:18 +0100

    gs for g2g

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -43,11 +43,11 @@ REFERENCE_POINTS: Final[dict[str, dict[str, tuple[float, float]]]] = {
 }
 
 PARAMETER_SEARCH_SPACE: Final[dict[str, list]] = {
-    'hidden_dim': [64, 128],
+    'hidden_dim': [32, 64],
     'n_readout_layers': [1, 2],
     'n_message_passings': [2,3],
     'n_fc_layers': [1, 2],
-    'dropout':  [0.0, 0.05, 0.1, 0.15],#[0.1, 0.2, 0.3, 0.4, 0.5],
+    'dropout':  [0.1, 0.2, 0.3, 0.4, 0.5],
     'mlp_dropout': [0.2, 0.4, 0.6, 0.8],
     'softmax_function': ['softmax', 'log_softmax', 'none'],
     'norm_fc_layers': ['batch', 'layer', 'none'],
